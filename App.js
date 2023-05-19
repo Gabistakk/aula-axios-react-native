4f71a5bad05e1bc24662905a1ef7dc86a68f9e14
gabistakk <gabistakk@gmail.com>
2023-05-18 22:23:45 -0300

    2.0 version with abilities

---
+++ App.js
@@ -0,0 +1,62 @@
+
+import { useEffect } from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import axios from 'axios';
+import { useState } from 'react';
+
+
+
+import Card from './assets/components/Card.jsx'
+
+
+export default function App() {
+
+  const [data, setData] = useState([])
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  getData = async () => {
+    try {
+      const res = await axios.get("https://valorant-api.com/v1/agents")
+      setData(res.data['data'])
+      setLoaded(true)
+    }
+    catch (err) {
+      alert(err.message)
+    }
+  }
+
+
+
+  return (
+    
+    <ScrollView>
+        <View style={styles.container}>
+        {
+          loaded && data.map((object) => {
+            return (
+              <Card id={object['uuid']} titulo={object['displayName']} descricao={object['description']} imagem={object['displayIcon']} habilidades={object['abilities']} />
+            )
+          })
+        }
+    </View>
+      </ScrollView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: '5%',
+    paddingTop: '20%',
+    gap: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+});
---
+++ assets/components/Card.jsx
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+
+
+export default function Card( props ){
+
+
+return(
+      <View style={styles.containerCard}  key={props.id}>
+      <Text style={{ fontSize: 35,fontWeight: 'bold', textAlign: 'center'}}>{props.titulo}</Text>
+      <Image style={styles.imagemVenda} source={{uri: props.imagem}}></Image>
+      <Text style={{ fontSize: 20}}>{props.descricao}</Text>
+      <Text style={{ fontSize: 30}}>Habilidades:</Text>
+      {
+        props.habilidades.map((habilidade) =>{
+          return(
+          <View>
+            <Text style={{ fontSize: 20}}>{habilidade['displayName']}:</Text>
+            <Text style={{ fontSize: 15}}>{habilidade['description']}</Text>
+            </View>
+          )
+        })
+      }
+    </View>
+
+)
+
+}
+
+const windowWidth = Dimensions.get('window').width;
+
+
+const styles = StyleSheet.create({
+  containerCard: {
+    width: windowWidth / 1.2,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap:30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 15,
+    marginTop: 15,
+    elevation: 24,
+  },
+
+  imagemVenda: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+    borderRadius: 10,
+    resizeMode: 'contain'
+  },
+
+});
